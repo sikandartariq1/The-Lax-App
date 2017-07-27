6e7154547d5201e0194cc0f8cc3c7ef3344bd8f7
Sikandar Tariq <sikandar.tariq@amblersaleem.com>
2017-07-27 12:25:47 +0500

    + Add Devise on OldSalt
      	modified:   Gemfile
    	modified:   app/models/old_salt.rb
    	modified:   config/environments/development.rb
    	new file:   config/initializers/devise.rb
    	new file:   config/locales/devise.en.yml
    	modified:   config/routes.rb
    	new file:   db/migrate/20170727065351_add_devise_to_old_salts.rb
    	modified:   db/schema.rb

--- app/models/old_salt.rb
+++ app/models/old_salt.rb
@@ -1,3 +1,7 @@
 class OldSalt < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable,
+         :recoverable, :rememberable, :trackable, :validatable
   validates :first_name, :last_name, presence: true
 end
