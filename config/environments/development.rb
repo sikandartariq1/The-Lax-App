c403747ca8fc4130d76d52ebd6944a0923f4c25c
Sikandar Tariq <sikandar.tariq@amblersaleem.com>
2017-07-31 16:44:38 +0500

    + Set Typo       modified:   config/environments/development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,7 +9,7 @@ Rails.application.configure do
   # default url
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { :address: 'localhost', port: 1025 }
+  config.action_mailer.smtp_settings = { address: 'localhost', port: 1025 }
 
   # Do not eager load code on boot.
   config.eager_load = false
