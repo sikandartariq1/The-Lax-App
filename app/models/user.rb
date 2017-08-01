47ecf2e31d726d2d0703e205cbded9dc205b5121
Sikandar Tariq <sikandar.tariq@amblersaleem.com>
2017-08-01 16:54:48 +0500

    + Add User Show       modified:   app/controllers/users_controller.rb       modified:   app/models/user.rb       new file:   app/views/users/show.html.erb       modified:   config/routes.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   mount_uploader :profile_picture, ProfilePictureUploader
+  enum user_type: [:player, :coach]
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
