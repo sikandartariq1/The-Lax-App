016338db08c90f1bb1c3f88c0a01e5f802c0db36
Sikandar Tariq <sikandar.tariq@amblersaleem.com>
2017-08-01 17:38:34 +0500

    + User Index       modified:   app/controllers/users_controller.rb       new file:   app/views/users/index.html.erb       modified:   config/routes.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,12 @@
 class UsersController < ApplicationController
+
+  USERS_PER_PAGE = 10
+
+  def index
+    @users = User.page(params[:page]).per(USERS_PER_PAGE)
+  end
   def show
     @user = User.find_by_id(params[:id])
+    redirect_to users_path, alert: "User not found !" unless @user
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   devise_for :old_salts
 
   resources :old_salts, only: [:index, :show, :edit, :update]
-  resources :users, only: [:show]
+  resources :users, only: [:index, :show]
 
   root 'old_salts#index'
 end
