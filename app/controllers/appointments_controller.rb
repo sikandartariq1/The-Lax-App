fcc96f15308ef7ca6f7de2c740bd8f9586ccd8ee
Sikandar Tariq <sikandar.tariq@amblersaleem.com>
2017-08-02 18:21:37 +0500

    + Appointment index for User & old Salt       modified:   app/controllers/appointments_controller.rb       new file:   app/views/appointments/_appointment.html.erb       new file:   app/views/appointments/index.html.erb       modified:   config/routes.rb

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,2 +1,7 @@
 class AppointmentsController < ApplicationController
+  APPOINTMENTS_PER_PAGE = 10
+  def index
+    current_entity = current_user || current_old_salt
+    @appointments = current_entity.appointments.page(params[:page]).per(APPOINTMENTS_PER_PAGE)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
+
   devise_for :users, controllers: {registrations: "registrations"}
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
   devise_for :old_salts
 
-  resources :old_salts, only: [:index, :show, :edit, :update]
-  resources :users, only: [:index, :show, :edit, :update]
+  resources :old_salts, only: [:index, :show, :edit, :update] do
+    resources :appointments, only: [:index]
+  end
+  resources :users, only: [:index, :show, :edit, :update] do
+    resources :appointments, only: [:index]
+  end
 
   root 'old_salts#index'
 end
