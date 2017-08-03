8a8e958aa28d67e424d1a3c0a25fd562ccdd7ff6
Sikandar Tariq <sikandar.tariq@amblersaleem.com>
2017-08-03 17:36:55 +0500

    + Change enum of appointment      modified:   app/models/appointment.rb

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,7 +2,16 @@ class Appointment < ApplicationRecord
   belongs_to :user
   belongs_to :old_salt
 
-  enum request_status: [:pending, :accepted, :rejected]
-  enum request_type: [:coaching, :analyzing]
+  enum status: {
+    pending: 0,
+    accepted: 1,
+    rejected: 2,
+  }
+
+  enum request_type: {
+    player_analysis: 0,
+    private_training: 1,
+    coaching_training: 2,
+  }
 
 end
