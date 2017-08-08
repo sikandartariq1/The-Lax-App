0fbecbfca8030b7da73bc1d1198d4ba88b67624d
Sikandar Tariq <sikandar.tariq@amblersaleem.com>
2017-08-08 17:35:52 +0500

    + Feedbacks Index       modified:   app/controllers/feedbacks_controller.rb       new file:   app/helpers/feedbacks_helper.rb       new file:   app/views/feedbacks/_feedback.html.erb       new file:   app/views/feedbacks/index.html.erb       modified:   config/routes.rb

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -1,4 +1,9 @@
 class FeedbacksController < ApplicationController
+  FEEDBACKS_PER_PAGE = 10
+
+  def index
+    @feedbacks = current_entity.feedbacks.page(params[:page]).per(FEEDBACKS_PER_PAGE)
+  end
 
   def show
     @feedback = current_entity.feedbacks.find(params[:id])
