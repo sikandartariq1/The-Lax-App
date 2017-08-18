c00a181bd5cfccf0f8ba8e948478dadfdfb4952b
Sikandar Tariq <sikandar.tariq@amblersaleem.com>
2017-08-18 18:20:46 +0500

    + Helper Function render-profile-picture_with class

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,6 +5,11 @@ module ApplicationHelper
     image_tag(path, class: "rounded float-left", size: size)
   end
 
+  def render_profile_picutre_with_class(resource, size, classes)
+    path = resource.profile_picture_url || '/default_avatar.gif'
+    image_tag(path, size: size, class: classes)
+  end
+
   def current_entity
     current_user || current_old_salt
   end
